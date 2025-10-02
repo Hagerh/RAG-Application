3df78bd567d00b47ab2bea60c85b7ff82f190dc5
Hager Hesham <126057015+Hagerh@users.noreply.github.com>
2025-10-03 01:02:45 +0300

    Update appRouter.dart

--- lib/appRouter.dart
+++ lib/appRouter.dart
@@ -1,18 +1,1 @@
-/*import 'package:flutter/material.dart';
 
-class AppRouter {
-
-  Route? generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case charactersScreen:
-        return MaterialPageRoute(
-          builder: (_) => BlocProvider(
-            create: (BuildContext context) => charactersCubit,
-            child: CharacterScreen(),
-          ),
-        );
-      case characterDetailsScreen:
-        return MaterialPageRoute(builder: (_) => CharactersDetailsScreen());
-    }
-  }
-}*/
